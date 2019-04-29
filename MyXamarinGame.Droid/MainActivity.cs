5bf2fc6ebe80f3f23009f9223bb813fb8ac671a3
slango0513 <tong.zhi.1989@gmail.com>
2019-04-30 00:44:40 +0800

    添加 Droid 项目。

---
+++ MyXamarinGame.Droid/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+using Xenko.Engine;
+using Xenko.Starter;
+using Android.Content.PM;
+
+namespace MyXamarinGame.Droid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true,
+          ScreenOrientation = ScreenOrientation.Landscape,
+              ConfigurationChanges = ConfigChanges.UiMode | ConfigChanges.Orientation | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenSize)]
+    public class MainActivity : AndroidXenkoActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            Game = new Game();
+            Game.Run(GameContext);
+        }
+    
+
+        protected override void OnDestroy()
+        {
+            Game.Dispose();
+
+            base.OnDestroy();
+        }
+    }
+}
